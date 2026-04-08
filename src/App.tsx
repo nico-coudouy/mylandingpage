60a70db719ba7a38a8e7ac2136edc2020dcac0b5
Nico <nicocoudouy@gmail.com>
2026-04-08 09:13:32 -0300

    LINKSS

--- src/App.tsx
+++ src/App.tsx
@@ -230,7 +230,7 @@ export default function App() {
                 title: "NEXCO",
                 desc: "Soluciones innovadoras en integración de medios y tecnología.",
                 icon: <Cpu className="text-blue-400" />,
-                color: "from-blue-500/20 to-transparent"
+                color: "from-blue-500/20 to-transparent",
                 link: "https://www.instagram.com/nexco.tech/"
               },
               {
